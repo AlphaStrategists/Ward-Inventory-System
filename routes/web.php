9da3cfd141a301f0276102a22dd87b9a9fb22023
gayanganynjth <gayangansilva@gmail.com>
2026-07-12 07:23:37 +0530

    Updated some files ✏️

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return view('components/login');
+});
+
+Route::get('/home', function() {
+    return view('components/home');
 });
