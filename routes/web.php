fd627667e6e587e3c97a1487f09100115446a886
Bhagy-1204 <hanipunibhagya1204@gmail.com>
2026-07-13 18:01:39 -0700

    Added Injection

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/inj', function () {
+    $drug = (object) [
+        'name' => 'Diazepam',
+        'stock_balance' => 85,
+        'unit' => 'tabs',
+    ];
+
+    return view('pages.Injection', ['drug' => $drug]);
+});
+Route::get('/injA', function () {
+  
+       
+
+    return view('pages.InjectionAntibiotic');
+});
