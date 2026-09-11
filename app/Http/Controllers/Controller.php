3e36d724d7ba32d00587ea8dfce00fa10e95fc72
gayanganynjth <gayangansilva@gmail.com>
2026-09-12 00:01:13 +0530

    Updated to version 2.0 🚀

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,5 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-<<<<<<< Updated upstream
     //
-=======
-    // Leave this empty or with your default logic
->>>>>>> Stashed changes
 }
--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,7 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-<<<<<<< Updated upstream
-=======
-use App\Http\Controllers\NarcoticController;
-use App\Http\Controllers\SyrupController;
-use App\Http\Controllers\IvFluidController;
-use App\Http\Controllers\OralCountableController;
-use App\Http\Controllers\AntibioticController;
-use App\Http\Controllers\OralAntibioticController;
-
-//Route::get('/', [Controller::class, 'index']);
-Route::get('/narcotics', [NarcoticController::class, 'index'])->name('narcotics.index');
-Route::get('/syrup', [SyrupController::class, 'index'])->name('syrup.index');
-Route::get('/iv-fluid', [IvFluidController::class, 'index'])->name('iv-fluid.index');
-Route::get('/oral-countable', [OralCountableController::class, 'index'])->name('oral-countable.index');
-Route::get('/antibiotic', [AntibioticController::class, 'index'])->name('antibiotic.index');
-Route::get('/oral-antibiotic', [OralAntibioticController::class, 'index'])->name('oral-antibiotic.index');
->>>>>>> Stashed changes
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/inj', function () {
-    $drug = (object) [
-        'name' => 'Diazepam',
-        'stock_balance' => 85,
-        'unit' => 'tabs',
-    ];
-
-    return view('pages.Injection', ['drug' => $drug]);
-});
-Route::get('/injA', function () {
-  
-       
-
-    return view('pages.InjectionAntibiotic');
-});
-Route::get('/', [UserController::class, 'login'])-> name('login');
-Route::get('/home', [UserController::class, 'home'])->name('home');
-Route::get('/password/reset', [UserController::class, 'showPasswordResetForm'])->name('password.request');
