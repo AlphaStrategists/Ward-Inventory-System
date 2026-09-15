a2b0a8ab0547cea4a726b63804cbcebc9ba39330
gayanganynjth <gayangansilva@gmail.com>
2026-09-15 09:44:21 +0530

    Homepage Initialization

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
-});
+    return view('home'); })->name('home');
+
+Route::get('/about', function () {
+    return view('about'); })->name('about');
+
+Route::get('/contact', function () {
+    return view('dashboard'); })->name('dashboard');
+
+    Route::get('/test', function() {
+        return view('test'); })->name('test');
