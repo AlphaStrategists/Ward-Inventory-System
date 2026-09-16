0e5be01dcdf2c454a0371d79303028f337c499fc
gayanganynjth <gayangansilva@gmail.com>
2026-09-16 09:39:26 +0530

    Added login page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div class="w-full h-full flex justify-center items-center">
+        <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+       
+            Home
+        </button>
+    </div>
+</body>
+</html>
